f8186d82c35a1be6b82f9715853a33245420bb65
Francisco <f.moya1991@gmail.com>
2015-09-23 17:46:42 +0200

    Dependencias e inicializacion

---
+++ app/src/main/java/com/android/app/slides/Hall.java
@@ -0,0 +1,12 @@
+package com.android.app.slides;
+
+/**
+ * Created by francisco on 23/9/15.
+ */
+public class Hall extends BaseActivity {
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.login;
+    }
+}
---
+++ app/src/main/java/com/android/app/slides/Register.java
@@ -0,0 +1,19 @@
+package com.android.app.slides;
+
+import android.os.Bundle;
+
+public class Register extends BaseActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.login;
+    }
+}
+

5094746e536324ca36a144cc146671785eda4328
dmerino76 <dmerino76@gmail.com>
2015-11-08 14:28:10 +0100

    Presentation Improvements

--- app/src/main/java/com/android/app/slides/activities/Presentation.java
+++ app/src/main/java/com/android/app/slides/activities/Presentation.java
@@ -3,9 +3,20 @@ package com.android.app.slides.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.android.app.slides.R;
+import com.android.app.slides.model.DAOSector;
+import com.android.app.slides.model.DAOUser;
+import com.android.app.slides.model.Sector;
+import com.android.app.slides.model.User;
+import com.android.app.slides.tools.SlidesApp;
 import com.gc.materialdesign.views.ButtonRectangle;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 
@@ -14,6 +25,13 @@ import butterknife.Bind;
  */
 public class Presentation extends BaseActivity {
     @Bind(R.id.UploadPptBtn) ButtonRectangle UploadPptBtn;
+
+    @Bind(R.id.userName)
+    TextView userName;
+    @Bind(R.id.userSector)
+    TextView userSector;
+
+    User user;
 
     @Override
     protected int getLayoutResource() {
@@ -31,5 +49,27 @@ public class Presentation extends BaseActivity {
                 startActivityForResult(fileIntent, RESULT_OK);
             }
         });
+
+        setUserInfo();
+    }
+
+    private void setUserInfo() {
+        DAOUser daoUser = new DAOUser(this);
+        user = daoUser.loadUser();
+
+        if (user != null) {
+            if (!user.getName().isEmpty()) {
+                userName.setText(user.getName());
+            }
+
+            DAOSector daoSector = new DAOSector(this);
+            ArrayList<Sector> sectors = daoSector.loadSectors();
+
+            if (!user.getSector().getName().isEmpty()) {
+                userSector.setText(user.getSector().getName());
+            }
+
+
+        }
     }
 }
